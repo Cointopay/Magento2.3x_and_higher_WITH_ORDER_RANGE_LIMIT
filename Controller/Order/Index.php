1c48d25ceef2b7a2ac0b132d1a237e5f42fc165a
Cointopaydev <info@cointopay.com>
2023-12-27 11:26:41 +0100

    Update Index.php

--- Controller/Order/Index.php
+++ Controller/Order/Index.php
@@ -15,7 +15,8 @@ class Index extends \Magento\Framework\App\Action\Action
     protected $_jsonEncoder;
     protected $orderManagement;
     protected $resultJsonFactory;
-	protected $resultFactory;
+    protected $resultFactory;
+    protected $invoice;
 	
 	/**
 	* @var \Magento\Sales\Model\Order\Email\Sender\InvoiceSender
